bc58bfb6383168c0466deae28ae00053e6f62e46
zap <109105581+zaplec@users.noreply.github.com>
2022-12-15 10:20:57 +0000

    Update louder.lua

--- louder.lua
+++ louder.lua
@@ -4,27 +4,3 @@ end
 if game.PlaceId == 2377868063 then -- Strucid
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/zaplec/zapp0s-hub/Strucid/strucid.lua'),true))()   
 end
-if game.PlaceId == 3297964905 then -- Weaponry
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Weaponry'),true))() 
-end
-if game.PlaceId == 621129760 then -- KAT
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/KAT'),true))() 
-end
-if game.PlaceId == 142823291 then -- MM2
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/MM2'),true))() 
-    end
-if game.PlaceId == 301549746 then -- CBRO
-   loadstring(game:HttpGet(('https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/CBRO'),true))()   
-end
-if game.PlaceId == 3233893879 then -- Bad Businesses
-   loadstring(game:HttpGet(('https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Bad%20Businesses'),true))()   
-end
-if game.PlaceId == 2788229376 then -- Da Hood
-   loadstring(game:HttpGet(('https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Da%20Hood'),true))()   
-end
-if game.PlaceId == 6172932937 then -- Energy Assault
-   loadstring(game:HttpGet(('https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Energy%20Assault'),true))()   
-end
-if game.PlaceId == 292439477 then -- Phantom Forces
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/PhantomForces'),true))()   
-end
