09d4058ed9f75dd24f2757ca3d4e496240ce42bf
zap <109105581+zaplec@users.noreply.github.com>
2022-12-15 19:53:21 +0000

    Update louder.lua

--- louder.lua
+++ louder.lua
@@ -1,8 +1,8 @@
 if game.PlaceId == 286090429 then -- Arsenal
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/zaplec/zapp0s-hub/Arsenal/Arsenal.lua'),true))()   
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/zaplec/zapp0s-hub/Arsenal/Arsenal.lua'), true))()   
 end
 if game.PlaceId == 2377868063 then -- Strucid
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/zaplec/zapp0s-hub/Strucid/strucid.lua'),true))()   
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/zaplec/zapp0s-hub/Strucid/strucid.lua'), true))()   
 end
 if game.PlaceId == 292439477 then -- Phantom Forces
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidMasterX/strawhook/main/script.lua", true))()
