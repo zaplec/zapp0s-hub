d66c084c29296fb06df1a1dcb6755c27cdaf5ee9
zap <109105581+zaplec@users.noreply.github.com>
2022-12-16 09:48:20 +0000

    Update manfuckthat.lua

--- manfuckthat.lua
+++ manfuckthat.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zaplec/zapp0s-hub/main/louder.lua"),true))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zaplec/zapp0s-hub/main/louder.lua", true))() 
